fa4aa3ba7966d47fa3e38d2f8d8f651d93cd970e
zackkh <44476801+zackkh@users.noreply.github.com>
2023-11-09 19:36:13 +0100

    Add function comments to methods

--- src/client.ts
+++ src/client.ts
@@ -2,14 +2,16 @@ import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 
 // Configuration interface for the AxiosClient class
 interface AxiosClientConfig<Aliases = Record<string, string>> {
-    config: AxiosRequestConfig; // Configuration options for Axios
-    aliases: Aliases; // Aliases for URL paths
+    // Configuration options for Axios
+    config: AxiosRequestConfig;
+    // Aliases for URL paths
+    aliases: Aliases;
 }
 
 // AxiosClient class for making HTTP requests
 export default class AxiosClient<Aliases = Record<string, string>> {
-    private client: AxiosInstance; // Axios HTTP client instance
-    private aliases: Aliases; // Aliases for URL paths
+    private client: AxiosInstance;
+    private aliases: Aliases;
 
     // Constructor for initializing the AxiosClient
     constructor(options: AxiosClientConfig<Aliases>) {
@@ -18,79 +20,116 @@ export default class AxiosClient<Aliases = Record<string, string>> {
         // Create an Axios instance with the provided configuration
         this.client = axios.create({
             ...config,
-            // proxy: false,
-            // withCredentials: true,
-            // paramsSerializer: { indexes: null },
         });
 
         this.aliases = aliases;
     }
 
-    // Function for making a GET request
+    /**
+     * Function for making a GET request
+     * @param path - Path alias
+     * @param params - Request parameters
+     * @param config - Additional request configuration
+     * @returns axios response
+     */
     async get<T>(
-        path: string & keyof Aliases, // URL path, possibly with an alias
-        params?: any, // Request parameters
-        config?: AxiosRequestConfig // Additional request configuration
+        path: string & keyof Aliases,
+        params?: any,
+        config?: AxiosRequestConfig
     ): Promise<AxiosResponse<T>> {
         const url = this.resolvePath(path, params);
         const response = await this.client.get<T>(url, config);
         return response;
     }
 
-    // Function for making a POST request
+    /**
+     * Function for making a POST request
+     * @param path - Path alias
+     * @param data - Request data
+     * @param config - Additional request configuration
+     * @returns axios response
+     */
     async post<T>(
-        path: string & keyof Aliases, // URL path, possibly with an alias
-        data?: any, // Request data
-        config?: AxiosRequestConfig // Additional request configuration
+        path: string & keyof Aliases,
+        data?: any,
+        config?: AxiosRequestConfig
     ): Promise<AxiosResponse<T>> {
         const url = this.resolvePath(path, data);
         const response = await this.client.post<T>(url, data, config);
         return response;
     }
 
-    // Function for making a PUT request
+    /**
+     * Function for making a PUT request
+     * @param path - Path alias
+     * @param data - Request data
+     * @param config - Additional request configuration
+     * @returns axios response
+     */
     async put<T>(
-        path: string & keyof Aliases, // URL path, possibly with an alias
-        data?: any, // Request data
-        config?: AxiosRequestConfig // Additional request configuration
+        path: string & keyof Aliases,
+        data?: any,
+        config?: AxiosRequestConfig
     ): Promise<AxiosResponse<T>> {
         const url = this.resolvePath(path, data);
         const response = await this.client.put<T>(url, data, config);
         return response;
     }
 
-    // Function for making a PATCH request
+    /**
+     * Function for making a PATCH request
+     * @param path - Path alias
+     * @param data - Request data
+     * @param config - Additional request configuration
+     * @returns axios response
+     */
     async patch<T>(
-        path: string & keyof Aliases, // URL path, possibly with an alias
-        data?: any, // Request data
-        config?: AxiosRequestConfig // Additional request configuration
+        path: string & keyof Aliases,
+        data?: any,
+        config?: AxiosRequestConfig
     ): Promise<AxiosResponse<T>> {
         const url = this.resolvePath(path, data);
         const response = await this.client.patch<T>(url, data, config);
         return response;
     }
 
-    // Function for making a DELETE request
+    /**
+     * Function for making a POST request
+     * @param path - Path alias
+     * @param params - Request parameters
+     * @param config - Additional request configuration
+     * @returns axios response
+     */
     async delete<T>(
-        path: string & keyof Aliases, // URL path, possibly with an alias
-        params?: any, // Request parameters
-        config?: AxiosRequestConfig // Additional request configuration
+        path: string & keyof Aliases,
+        params?: any,
+        config?: AxiosRequestConfig
     ): Promise<AxiosResponse<T>> {
         const url = this.resolvePath(path, params);
         const response = await this.client.delete<T>(url, config);
         return response;
     }
 
-    // Private function to resolve the actual URL path with aliases
+    /**
+     * Private function to resolve the actual URL path with aliases
+     * @param path - Path alias
+     * @param params - Request parameters
+     * @returns interpolated path
+     */
     private resolvePath(path: string & keyof Aliases, params?: any): string {
         const aliasedPath = (this.aliases[path] || path) as string;
         return this.interpolateParams(aliasedPath, params);
     }
 
-    // Private function to interpolate path parameters
+    /**
+     * Private function to interpolate path parameters
+     * @param path - URL path with placeholders
+     * @param params - Request parameters
+     * @returns interpolated path
+     */
     private interpolateParams(
-        path: string, // URL path with placeholders
-        params?: object | FormData // Request parameters
+        path: string,
+        params?: object | FormData
     ): string {
         if (!params) {
             return path;
--- src/response.ts
+++ src/response.ts
@@ -1,15 +1,27 @@
 import { AxiosError, AxiosResponse } from "axios";
 
+/**
+ * Check wether axios response has a successful http status code
+ * @param response - an instance of axios response
+ */
 export function isResponseSuccessful<T>(
     response?: AxiosResponse<T> | null
 ): boolean {
     return response ? response.status >= 200 && response.status < 300 : false;
 }
 
+/**
+ * Check wether axios response has an error http status code
+ * @param response - an instance of axios response
+ */
 export function isResponseFail<T>(response?: AxiosResponse<T> | null): boolean {
     return response ? response.status >= 400 : false;
 }
 
+/**
+ * Adds a response object if error is missing one
+ * @param error - an instance of axios error
+ */
 export function fixErrorResponse(error: AxiosError): AxiosError {
     if (error.response) {
         return error;
